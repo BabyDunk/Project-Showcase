f2d0a13c26ac56432453477e65c99b424ac6c871
Chris Wilkison <chris@babydunk.co.uk>
2019-04-23 18:29:49 +0100

    config bug fix

--- includes/config.php
+++ includes/config.php
@@ -1,15 +1,3 @@
 <?php
 
 
-// Database Credentials
-$DB_HOST = "localhost";
-$DB_USER = "c1_scdemo01";
-$DB_PASS = "gdrLvdPA4WH#gdrLvdPA4WH#";
-$DB_NAME = "c1_scdemo01";
-$DB_CHARSET = "utf8mb4";
-$DB_PREFIX = "sc_";
-
-// Composer AutoLoader
-require SITE_ROOT . "vendor/autoload.php";
-
-$IS_INSTALLED = true;
